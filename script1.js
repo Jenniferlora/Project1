5291e41360623d47c1a33bf3b6c1615fcc6e00a8
Jenniferlora <lorajennifer@aol.com>
2018-01-08 14:08:16 -0500

    first commit

--- script1.js
+++ script1.js
@@ -1,199 +1,193 @@
-var alphabet = 'abcdefghijklmnopqrstuvwxyz'; //alphabet
-var letters = alphabet.split(''); //splits into an array
-var wrongLetter = 0; //counter for how many wong letters were clicked
-var clicked; //clicked item
-var currentWord; //current word
-var goodmove = 0; //counter for each good move
-var badmove = 0; //counter for each bad move
-var currentLength; //length of current word
-var applause = new Audio('./sound/lightapplause.mp3');
-var right = new Audio('./sound/right.wav');
-var wrong = new Audio('./sound/wrong.wav');
-var points = new Audio('./sound/points.wav');
-var won = 0;
-var loss = 0;
-var turn = 0;
-//wordbank
-var words = [
-	'snag',
-	'mime',
-	'jungle',
-	'peasant',
-	'password',
-	'newsletter',
-	'bookend',
-	'fly',
-	'fang',
-	'bicycle',
-	'bear',
-	'cape',
-	'puppet',
-	'piano',
-	'lipstick',
-	'salute',
-];
-//container for letters
-var $letterBox = $('#letterBox');
+window.onload = function(e) {
+	var alphabet = 'abcdefghijklmnopqrstuvwxyz'; //alphabet
+	var letters = alphabet.split(''); //splits into an array
+	var wrongLetter = 0; //counter for how many wong letters were clicked
+	var clicked; //clicked item
+	var currentWord; //current word
+	var goodmove = 0; //counter for each good move
+	var badmove = 0; //counter for each bad move
+	var currentLength; //length of current word
+	var applause = new Audio('./sound/lightapplause.mp3');
+	var right = new Audio('./sound/right.wav');
+	var wrong = new Audio('./sound/wrong.wav');
+	var points = new Audio('./sound/points.wav');
+	var won = 0;
+	var loss = 0;
+	var turn = 0;
 
-//This function will make a new div for each letter,display the letter, and appended to letterBox
-function getLetters() {
-	letters.forEach(function(letter) {
-		var $eachLetter = $('<div>');
-		$eachLetter.attr('class', 'letters el');
-		$eachLetter.text(letter);
-		$eachLetter.css('cursor', 'pointer');
-		$eachLetter.on('click', play);
-		$letterBox.append($eachLetter);
-	});
-}
+	var $letterBox = $('#letterBox');
 
-getLetters();
-
-//This function will change the pictures to resemble a limb added.
-function addLimbs() {
-	var hangman = $('#hangman');
-	wrongLetter++;
-	hangman.attr('src', './images/Hangman' + wrongLetter + '.png');
-}
-
-//Generates a random word.
-function getWord() {
-	var r = Math.floor(Math.random() * words.length);
-	currentWord = words[r].toLowerCase();
-	getDashes();
-}
-getWord();
-
-//This function will make placeholders with dashes based on length of word.
-function getDashes() {
-	currentLength = currentWord.length;
-	var splitWord = currentWord.split('');
-	splitWord.forEach(function(letter) {
-		var $space = $('<div>');
-		$space.attr('data', letter);
-		$space.addClass('spaces el');
-		$space.css('border-bottom', '2px solid black');
-		var $dashes = $('#letterdash');
-		$dashes.append($space);
-	});
-}
-
-// This function will write the letter in each space where it belongs.
-function writeLetter() {
-	var $spaces = $('.spaces');
-	$spaces.each(function(space) {
-		$this = $(this);
-		if ($this.attr('data') == clicked) {
-			$this.text(clicked);
-			right.play();
-			goodmove++; //needs to be here so it can capture any instance of multiple letters
-			setTimeout(checkForScore(), 2000); //check for win conditions
-		}
-	});
-}
-
-//This function checks to see if the current word includes the letter that was clicked,
-//and fires the appropiate next step functions.
-function play(event) {
-	clicked = event.target.textContent;
-
-	console.log(currentWord);
-	console.log(clicked);
-
-	event.target.remove(); //removes letter from dashboard after it's been clicked.
-
-	if (currentWord.includes(clicked)) {
-		console.log('Hey,I wrote a letter');
-		writeLetter(); //writes letter
-	} else {
-		wrong.play();
-		badmove++; //increment bad move count
-		addLimbs(); //change hangman picture
-		console.log('Hey,I added a Limb');
-		lost(); //alerts user they lost
+	//This function will make a new div for each letter,display the letter, and appended to letterBox
+	function getLetters() {
+		letters.forEach(function(letter) {
+			var $eachLetter = $('<div>');
+			$eachLetter.attr('class', 'letters el');
+			$eachLetter.text(letter);
+			$eachLetter.css('cursor', 'pointer');
+			$eachLetter.on('click', play);
+			$letterBox.append($eachLetter);
+		});
 	}
-}
 
-//This function alerts the user if they won.
-function checkForScore() {
-	if (goodmove >= currentLength) {
-		won++;
-		scoreBoard();
-		points.play();
+	getLetters();
 
-		setTimeout(function() {
-			alert(`"That's correct, Good Job!"`);
-		}, 100);
-
-		setTimeout(function() {
-			clear();
-		}, 200);
-		turn++;
-		setTimeout(function() {
-			checkforWin();
-		}, 300);
+	//This function will change the pictures to resemble a limb added.
+	function addLimbs() {
+		var hangman = $('#hangman');
+		wrongLetter++;
+		hangman.attr('src', './images/Hangman' + wrongLetter + '.png');
 	}
-}
 
-//This function alerts the user if they lost.
-function lost() {
-	if (badmove == 6) {
-		loss++;
-		scoreBoard();
-		setTimeout(function() {
-			alert(`That's wrong! The answer was ` + currentWord + '.');
-		}, 100);
-
-		setTimeout(function() {
-			clear();
-		}, 200);
-		turn++;
-		setTimeout(function() {
-			checkforWin();
-		}, 300);
+	//Generates a random word.
+	function getWord() {
+		var r = Math.floor(Math.random() * words.length);
+		currentWord = words[r].word;
+		currentHint = words[r].hint;
+		getDashes();
 	}
-}
+	getWord();
 
-//This function starts the game.
-function start() {
-	getLetters(); //generates all new letters.
-	getWord(); //generates new random word.
-}
+	//This function will make placeholders with dashes based on length of word.
+	function getDashes() {
+		currentLength = currentWord.length;
+		var splitWord = currentWord.split('');
+		splitWord.forEach(function(letter) {
+			var $space = $('<div>');
+			$space.attr('data', letter);
+			$space.addClass('spaces el');
+			$space.css('border-bottom', '2px solid black');
+			var $dashes = $('#letterdash');
+			$dashes.append($space);
+		});
+	}
 
-//This function clears the letterbox, the dahed spaces, and image, to their initial values.
-function clear() {
-	badmove = 0;
-	goodmove = 0;
+	// This function will write the letter in each space where it belongs.
+	function writeLetter() {
+		var $spaces = $('.spaces');
+		$spaces.each(function(space) {
+			$this = $(this);
+			if ($this.attr('data') == clicked) {
+				$this.text(clicked);
+				right.play();
+				goodmove++; //needs to be here so it can capture any instance of multiple letters
+				setTimeout(checkForScore(), 2000); //check for win conditions
+			}
+		});
+	}
 
-	var letterbox = document.querySelector('#letterBox');
-	letterbox.innerHTML = '';
+	//This function checks to see if the current word includes the letter that was clicked,
+	//and fires the appropiate next step functions.
+	function play(event) {
+		clicked = event.target.textContent;
 
-	var letterdash = document.querySelector('#letterdash');
-	letterdash.innerHTML = '';
+		console.log(currentWord);
+		console.log(clicked);
 
-	wrongLetter = 0;
-	var hangman = $('#hangman');
-	hangman.attr('src', './images/Hangman' + wrongLetter + '.png');
+		event.target.remove(); //removes letter from dashboard after it's been clicked.
 
-	start();
-}
-var $won = $('#won');
-var $loss = $('#loss');
-
-function scoreBoard() {
-	var winning = 'won:' + won;
-	var losing = 'lost:' + loss;
-	$won.text(winning);
-	$loss.text(losing);
-}
-
-function checkforWin() {
-	if (turn == 3) {
-		if (won >= 2) {
-			applause.play();
-			alert('You won the game!');
+		if (currentWord.includes(clicked)) {
+			console.log('Hey,I wrote a letter');
+			writeLetter(); //writes letter
 		} else {
-			alert('You lost the game');
+			wrong.play();
+			badmove++; //increment bad move count
+			addLimbs(); //change hangman picture
+			console.log('Hey,I added a Limb');
+			lost(); //alerts user they lost
 		}
 	}
-}
+
+	//This function alerts the user if they won.
+	function checkForScore() {
+		if (goodmove >= currentLength) {
+			won++;
+			scoreBoard();
+			points.play();
+
+			setTimeout(function() {
+				alert(`"That's correct, Good Job!"`);
+			}, 100);
+
+			setTimeout(function() {
+				clear();
+			}, 200);
+			turn++;
+			setTimeout(function() {
+				checkforWin();
+			}, 300);
+		}
+	}
+
+	//This function alerts the user if they lost.
+	function lost() {
+		if (badmove == 6) {
+			loss++;
+			scoreBoard();
+			setTimeout(function() {
+				alert(`That's wrong! The answer was ` + currentWord + '.');
+			}, 100);
+
+			setTimeout(function() {
+				clear();
+			}, 200);
+			turn++;
+			setTimeout(function() {
+				checkforWin();
+			}, 300);
+		}
+	}
+
+	//This function starts the game.
+	function start() {
+		getLetters(); //generates all new letters.
+		getWord(); //generates new random word.
+	}
+
+	//This function clears the letterbox, the dahed spaces, and image, to their initial values.
+	function clear() {
+		badmove = 0;
+		goodmove = 0;
+
+		var letterbox = document.querySelector('#letterBox');
+		letterbox.innerHTML = '';
+
+		var letterdash = document.querySelector('#letterdash');
+		letterdash.innerHTML = '';
+
+		wrongLetter = 0;
+		var hangman = $('#hangman');
+		hangman.attr('src', './images/Hangman' + wrongLetter + '.png');
+
+		start();
+	}
+	var $won = $('#won');
+	var $loss = $('#loss');
+
+	function scoreBoard() {
+		var winning = 'won:' + won;
+		var losing = 'lost:' + loss;
+		$won.text(winning);
+		$loss.text(losing);
+	}
+
+	function checkforWin() {
+		if (turn == 3) {
+			if (won >= 2) {
+				applause.play();
+				resetScores();
+
+				alert('You won the game!');
+			} else {
+				resetScores();
+				alert('You lost the game');
+			}
+		}
+	}
+
+	function resetScores() {
+		turn = 0;
+		won = 0;
+		loss = 0;
+		scoreBoard();
+	}
+};
---
+++ words1.js
@@ -0,0 +1,380 @@
+//This file contains the wordbank.
+var words = [
+	{
+		word: 'mime',
+		hint: 'entertainer',
+		difficulty: 'medium',
+	},
+	{
+		word: 'twist',
+		hint: 'action',
+		difficulty: 'medium',
+	},
+	{
+		word: 'rhinoceros',
+		hint: 'animal',
+		difficulty: 'difficult',
+	},
+	{
+		word: 'cow',
+		hint: 'animal',
+		difficulty: 'easy',
+	},
+	{
+		word: 'tickle',
+		hint: 'makes you giggle',
+		difficulty: 'medium',
+	},
+	{
+		word: 'fetch',
+		hint: 'similar to acquire',
+		difficulty: 'medium',
+	},
+	{
+		word: 'violin',
+		hint: 'music',
+		difficulty: 'medium',
+	},
+	{
+		word: 'anaconda',
+		hint: 'animal',
+		difficulty: 'hard',
+	},
+	{
+		word: 'cage',
+		hint: 'locked',
+		difficulty: 'easy',
+	},
+	{
+		word: 'cello',
+		hint: 'music',
+		difficulty: 'medium',
+	},
+	{
+		word: 'braid',
+		hint: 'appearance',
+		difficulty: 'medium',
+	},
+	{
+		word: 'skateboard',
+		hint: 'transportation',
+		difficulty: 'difficult',
+	},
+	{
+		word: 'stairs',
+		hint: 'building',
+		difficulty: 'difficult',
+	},
+	{
+		word: 'trumpet',
+		hint: 'music',
+		difficulty: 'medium',
+	},
+	{
+		word: 'mop',
+		hint: 'clean',
+		difficulty: 'difficult',
+	},
+	{
+		word: 'shovel',
+		hint: 'tool',
+		difficulty: 'difficult',
+	},
+	{
+		word: 'money',
+		hint: 'trade',
+		difficulty: 'difficult',
+	},
+	{
+		word: 'soap',
+		hint: 'clean',
+		difficulty: 'easy',
+	},
+	{
+		word: 'saddle',
+		hint: 'horseback',
+		difficulty: 'easy',
+	},
+	{
+		word: 'wink',
+		hint: 'emoji',
+		difficulty: 'easy',
+	},
+	{
+		word: 'spider',
+		hint: 'animal',
+		difficulty: 'medium',
+	},
+	{
+		word: 'salute',
+		hint: 'greeting',
+		difficulty: 'medium',
+	},
+	{
+		word: 'peasant',
+		hint: 'poor',
+		difficulty: 'medium',
+	},
+	{
+		word: 'lipstick',
+		hint: 'cosmetics',
+		difficulty: 'medium',
+	},
+	{
+		word: 'piano',
+		hint: 'music',
+		difficulty: 'easy',
+	},
+	{
+		word: 'puppet',
+		hint: 'theater',
+		difficulty: 'difficult',
+	},
+	{
+		word: 'cape',
+		hint: 'costume',
+		difficulty: 'medium',
+	},
+	{
+		word: 'bookend',
+		hint: 'book',
+		difficulty: 'medium',
+	},
+	{
+		word: 'jungle',
+		hint: 'place',
+		difficulty: 'difficult',
+	},
+	{
+		word: 'roof',
+		hint: 'building',
+		difficulty: 'easy',
+	},
+	{
+		word: 'penguin',
+		hint: 'animal',
+		difficulty: 'medium',
+	},
+	{
+		word: 'banana',
+		hint: 'food',
+		difficulty: 'medium',
+	},
+	{
+		word: 'whisper',
+		hint: 'action',
+		difficulty: 'difficult',
+	},
+	{
+		word: 'popsicle',
+		hint: 'food',
+		difficulty: 'difficult',
+	},
+	{
+		word: 'frankestein',
+		hint: 'character',
+		difficulty: 'difficult',
+	},
+	{
+		word: 'earthquake',
+		hint: 'natural events',
+		difficulty: 'difficult',
+	},
+	{
+		word: 'rain',
+		hint: 'weather',
+		difficulty: 'easy',
+	},
+	{
+		word: 'alarm',
+		hint: 'rings',
+		difficulty: 'easy',
+	},
+	{
+		word: 'dog',
+		hint: 'animal',
+		difficulty: 'easy',
+	},
+	{
+		word: 'leash',
+		hint: 'item',
+		difficulty: 'easy',
+	},
+	{
+		word: 'pajamas',
+		hint: 'item',
+		difficulty: 'medium',
+	},
+];
+
+// fiddle seashell jog
+// seesaw nap cheerleader
+// blind beg shopping cart panda housecat moth
+// moose water buffalo caterpillar
+// albatross duck mouse
+// salamander Canadian goose rat
+// aardvark joey robin
+// horse koala black bear
+// hippopotamus camel cougar
+// giraffe bald eagle crocodile
+// dog bird penguin
+// pony bat otter
+// porcupine dolphin hamster
+// chameleon swordfish groundhog
+// cricket clownfish gila monster
+// leopard rabbit buffalo
+// shark stallion wombat
+// kitten reindeer wolf
+// dragon blue whale hyena
+// unicorn tuna platypus
+// basset hound shrimp skunk
+// bulldog ladybug beaver
+// puppy weasel owl
+// chipmunk lamb tiger
+// doe ewe pelican
+// ape sheep dog partridge
+// hedgehog cow spider
+// turtle alligator seahorse
+// sea turtle llama hive
+// lion armadillo herd
+// gopher three - toed sloth elk
+// fox guinea pig sea lion
+// donkey frog crow
+// zebra toad worm
+// panther chinchilla flock
+// chimpanzee calf school of fish
+// warthog cub seagull
+// parakeet chicken human
+// toucan deer lobster
+// prairie dog dove walrus
+// komodo dragon jellyfish crab
+// tortoise duckling wasp
+// squirrel wildebeest catfish
+// Egypt skiing the Bahamas
+// luggage Europe beach house
+// packing camping rainforest
+// Colorado river airport
+// summer Canada sunscreen
+// Disneyland passport Arizona
+// Aruba sightseeing resort
+// water park customs America
+// Africa tourist island
+// lake sunny weather airport security
+// cruise ship Mexico sleeping bag
+// hotel mountain biking Paris
+// road trip Bermuda Jamaica
+// Spain snowboarding Say "cheese"
+// map trains Alaska
+// camera theme park national park
+// tour guide relax Delaware
+// airplane bug spray scenery
+// layover Are we there yet ? fanny pack
+// river rafting beach ball kayak
+// Catchphrase Words: Travel
+// Copyright© 2012 The Game Gal | www.thegamegal.com
+// movies hiking stroll
+// surfboard time off swimming pool
+// bird watching Broadway Universal Studios
+// binoculars shopping Rome
+// Maine bicycle snow cone
+// vacation day boardwalk Six Flags
+// airline lazy river Florida
+// Texas South Dakota Istanbul
+// the Grand Canyon June holiday
+// safari Las Vegas suitcase
+// Turkey hang glider first class
+// condo honeymoon scuba diving
+// China Africa Italy
+// Hawaii family reunion Scottland
+// London Four Corners backpacking
+// Ireland California Switzerland
+// July Tokyo Mount Rushmore
+// coral reef summer France
+// San Francisco Athens Vienna
+// England Washington D.C.money belt
+// Los Angeles Boston party Egypt skiing the Bahamas
+// luggage Europe beach house
+// packing camping rainforest
+// Colorado river airport
+// summer Canada sunscreen
+// Disneyland passport Arizona
+// Aruba sightseeing resort
+// water park customs America
+// Africa tourist island
+// lake sunny weather airport security
+// cruise ship Mexico sleeping bag
+// hotel mountain biking Paris
+// road trip Bermuda Jamaica
+// Spain snowboarding Say "cheese"
+// map trains Alaska
+// camera theme park national park
+// tour guide relax Delaware
+// airplane bug spray scenery
+// layover Are we there yet ? fanny pack
+// river rafting beach ball kayak
+
+// movies hiking stroll
+// surfboard time off swimming pool
+// bird watching Broadway Universal Studios
+// binoculars shopping Rome
+// Maine bicycle snow cone
+// vacation day boardwalk Six Flags
+// airline lazy river Florida
+// Texas South Dakota Istanbul
+// the Grand Canyon June holiday
+// safari Las Vegas suitcase
+// Turkey hang glider first class
+// condo honeymoon scuba diving
+// China Africa Italy
+// Hawaii family reunion Scottland
+// London Four Corners backpacking
+// Ireland California Switzerland
+// July Tokyo Mount Rushmore
+// coral reef summer France
+// San Francisco Athens Vienna
+// England Washington D.C.money belt
+// Los Angeles Boston party
+
+// hard words
+// whatever buddy sip
+// chicken coop blur chime
+// bleach clay blossom
+// cog twitterpated wish
+// through feudalism whiplash
+// cot blueprint beanstalk
+// think cardboard darts
+// inn Zen crow 's nest
+// BFF sheriff tiptop
+// dot bob garden hose
+// blimp dress shirt reimbursement
+// capitalism step - daughter applause
+// jig jade blunt
+// application rag squint
+// intern sow 's ear brainstorm
+// sling half pinch
+// leak skating rink jog
+// jammin ' shrink ray dent
+// scoundrel escalator cell phone charger
+// kitchen knife set sequins ladder rung
+// Catchphrase Words: Hard
+// Copyright© 2012 The Game Gal | www.thegamegal.com
+// flu scuff mark mast
+// sash modern ginger
+// clockwork mess mascot
+// runt chain scar tissue
+// suntan pomp scramble
+// sentence first mate cuff
+// cuticle fortnight riddle
+// spool full moon forever
+// rut hem new
+// freight train diver fringe
+// humidifier handwriting dawn
+// dimple gray hairs hedge
+// plank race publisher
+// fizz gem ditch
+// wool plaid fancy
+// ebony and ivory feast Murphy 's Law
+// billboard flush inconceivable
+// tide midsummer population
+// my elm organ
+// flannel hatch booth
